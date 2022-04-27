3aaf94f7a68600a4dcc739af4d730256ef9c4941
JohnyDeCoder <johnymora251@gmail.com>
2022-04-26 22:34:18 -0500

    Added project and class folder

---
+++ rootprox-2022/Forms/ROOTPROX - Inicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rootprox_2022
+{
+    public partial class ROOTPROX_Inicio : Form
+    {
+        public ROOTPROX_Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void ROOTPROX_Inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
